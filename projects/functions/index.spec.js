1e568cc489e7162d988fe2a869f61fe397328b81
s4int <s4int@bk.ru>
2022-07-24 02:29:44 +0500

    homework+

--- projects/functions/index.spec.js
+++ projects/functions/index.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { randomStringArray, randomValue as random } from '../../scripts/helper';
 import {
   bindFunction,
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,5 +1,3 @@
-global.fetch = require('node-fetch-polyfill');
-
 const homeworkContainer = document.createElement('div');
 
 homeworkContainer.id = 'homework-container';
